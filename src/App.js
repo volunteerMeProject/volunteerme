5aa97b03d73782bef593b616d2fa7e2569e45582
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2021-03-18 22:38:37 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './component/SignIn';
+import SignUp from './component/SignUp';
+import Dashboard from './component/Dashboard';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+ 
+
+
+function App() {
+  return (
+      <Router>
+        <Route exact path="/" component={SignIn}/>
+        <Route path="/signup" component={SignUp}/>
+        <Route path="/dashboard" component={Dashboard}/>
+      </Router>
+  );
+}
+
+export default App;
